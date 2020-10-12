f43a234eee765ac3b4f4d4f3976308fbea5a36d9
Hrithikraj2 <61221919+Hrithikraj2@users.noreply.github.com>
2020-10-13 04:05:15 +0530

    Add files via upload

--- Assignment_5/A5.py
+++ Assignment_5/A5.py
@@ -1,215 +1,37 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Oct  8 18:05:30 2020
+Created on Tue Oct 13 03:23:26 2020
 
 @author: Hrithik
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-def dir_vec(A,B):
-  return B-A
-
-def norm_vec(A,B):
-  return np.matmul(omat, dir_vec(A,B))
-
-#Generate line points
-#def line_gen(A,B):
-#  len =10
-#  dim = A.shape[0]
-#  x_AB = np.zeros((dim,len))
-#  lam_1 = np.linspace(0,1,len)
-#  for i in range(len):
-#    temp1 = A + lam_1[i]*(B-A)
-#    x_AB[:,i]= temp1.T
-#  return x_AB
-
-#Generate line intercepts
-def line_icepts(n,c):
-  e1 = np.array([1,0]) 
-  e2 = np.array([0,1]) 
-  A = c*e1/(n@e1)
-  B = c*e2/(n@e2)
-  return A,B
-
-#Generate line points
-def line_dir_pt(m,A,k1,k2):
-  len =10
-  m1=np.matmul(omat, m)
-  dim = A.shape[0]
-  x_AB = np.zeros((dim,len))
-  lam_1 = np.linspace(k1,k2,len)
-  for i in range(len):
-    temp1 = A + lam_1[i]*m1
-    x_AB[:,i]= temp1.T
-  return x_AB
-#Generate line points
-
-def line_norm_eq(n,c,k):
-  len =10
-  dim = n.shape[0]
-  m = omat@n
-  m = m/np.linalg.norm(m)
-#  x_AB = np.zeros((dim,2*len))
-  x_AB = np.zeros((dim,len))
-  lam_1 = np.linspace(k[0],k[1],len)
-#  print(lam_1)
-#  lam_2 = np.linspace(0,k2,len)
-  if c==0:
-    for i in range(len):
-      temp1 = lam_1[i]*m
-      x_AB[:,i]= temp1.T
-  else:
-    A,B = line_icepts(n,c)
-    for i in range(len):
-      temp1 = A + lam_1[i]*m
-      x_AB[:,i]= temp1.T
-#    temp2 = B + lam_2[i]*m
-#    x_AB[:,i+len]= temp2.T
-  return x_AB
-
-#def line_dir_pt(m,A, dim):
-#  len = 10
-#  dim = A.shape[0]
-#  x_AB = np.zeros((dim,len))
-#  lam_1 = np.linspace(0,10,len)
-#  for i in range(len):
-#    temp1 = A + lam_1[i]*m
-#    x_AB[:,i]= temp1.T
-#  return x_AB
+import math
 
 
-#Generate line points
-def line_gen(A,B):
-  len =10
-  x_AB = np.zeros((2,len))
-  lam_1 = np.linspace(0,1,len)
-  for i in range(len):
-    temp1 = A + lam_1[i]*(B-A)
-    x_AB[:,i]= temp1.T
-  return x_AB
 
-#Foot of the Altitude
-def alt_foot(A,B,C):
-  m = B-C
-  n = np.matmul(omat,m) 
-  N=np.vstack((m,n))
-  p = np.zeros(2)
-  p[0] = m@A 
-  p[1] = n@B
-  #Intersection
-  P=np.linalg.inv(N.T)@p
-  return P
+# equation - y^2 = 4x
+X_coord = np.array(np.linspace(0, 5.0, num=40))
+Y_coord =[math.sqrt(4*num) for num in X_coord]
+y1 = np.zeros(len(X_coord))
 
-#Intersection of two lines
-def line_intersect(n1,c1,n2,c2):
-  N=np.vstack((n1,n2))
-  p = np.array([c1,c2]) 
-  #Intersection
-  P=np.linalg.inv(N)@p
-#  P=np.linalg.inv(N.T)@p
-  return P
-
-#Radius and centre of the circumcircle
-#of triangle ABC
-def ccircle(A,B,C):
-  p = np.zeros(2)
-  n1 = dir_vec(B,A)
-  p[0] = 0.5*(np.linalg.norm(A)**2-np.linalg.norm(B)**2)
-  n2 = dir_vec(C,B)
-  p[1] = 0.5*(np.linalg.norm(B)**2-np.linalg.norm(C)**2)
-  #Intersection
-  N=np.vstack((n1,n2))
-  O=np.linalg.inv(N)@p
-  r = np.linalg.norm(A -O)
-  return O,r
-
-#Radius and centre of the incircle
-#of triangle ABC
-def icentre(A,B,C,k1,k2):
-  p = np.zeros(2)
-  t = norm_vec(B,C)
-  n1 = t/np.linalg.norm(t)
-  t = norm_vec(C,A)
-  n2 = t/np.linalg.norm(t)
-  t = norm_vec(A,B)
-  n3 = t/np.linalg.norm(t)
-  p[0] = n1@B- k1*n2@C
-  p[1] = n2@C- k2*n3@A
-  N=np.vstack((n1-k1*n2,n2-k2*n3))
-  I=np.matmul(np.linalg.inv(N),p)
-  r = n1@(I-B)
-  #Intersection
-  return I,r
-
-def mult_line(A_I,b_z,k,m):
- for i in range(m):
-  if i == 0:
-    x = line_norm_eq(A_I[i,:],b_z[i],k[i,:])
-  elif i == 1:
-    y = line_norm_eq(A_I[i,:],b_z[i],k[i,:])
-    z = np.vstack((x[None], y[None]))
-  else:
-    x = line_norm_eq(A_I[i,:],b_z[i],k[i,:])
-    z = np.vstack((z,x[None]))
- return z
-
-dvec = np.array([-1,1]) 
-#Orthogonal matrix
-omat = np.array([[0,1],[-1,0]]) 
-
-n1 = np.array([1,-1])
-n2 = np.array([3,2])
-n3 = np.array([0,1])
-c =  np.array([-1,12])
-
-c1 = -1
-c2 = 12
-c3 = 0
-#Intercept 1
-e1 = np.array([1,0]) 
-e2 = np.array([0,1]) 
-A1= c1*e1/(n1@e1)
-B1 = c1*e2/(n1@e2)
-k1 = 0
-k2 = 3
-N=np.vstack((n1,n2))
-D1=np.linalg.inv(N)@c
-x_A1B1 =  line_dir_pt(n1,D1,k1,k2)
-plt.plot(x_A1B1[0,:],x_A1B1[1,:],label='$x-y+1=0 $')
-
-#Intercept2
-A2 = c2*e1/(n2@e1)
-B2 = c2*e2/(n2@e2)
-x_A2B2 =   line_gen(A2,B2)
-plt.plot(x_A2B2[0,:],x_A2B2[1,:],label='$3x+2y-12=0$')
+for i in range(len(X_coord)):
+    y1[i] = Y_coord[i]*-1
 
 
-#Intersection of the lines
-A=line_intersect(n1,c1,n2,c2)
-B=line_intersect(n2,c2,n3,c3)
-C=line_intersect(n3,c3,n1,c1)
+#plot points and lines
+plt.plot(X_coord, Y_coord,'r', label='$y^2=4x$')
+plt.plot(X_coord, y1,'r')
+plt.plot(1, 2, 'o', label='Point of contact q=(1,2)')
+X_coord = np.array(np.linspace(-3, 4.0, num=40))
+y2= 1 + X_coord
+plt.plot(X_coord,y2,  label='y = x + 1 ') #Since m = 1   
+#plt.text(1, 2)
 
-points = np.array((A,B,C))
 
-#Filling up the desired region
-plt.fill(points[:,0], points[:,1], alpha=0.5)
 
-#Plotting points of Intersection
-plt.plot(A[0], A[1], 'o')
-plt.text(A[0] * (1 + 0.1), A[1] * (1 ) , 'A')
-plt.plot(B[0], B[1], 'o')
-plt.text(B[0] * (1 ), B[1] * (1)+0.1 , 'B')
-plt.plot(C[0], C[1], 'o')
-plt.text(C[0] * (1 - 0.05), C[1] * (1 - 0.1) , 'C')
-plt.plot(B2[0], B2[1], 'o')
-plt.text(B2[0] * (1 ), B2[1] * (1)+0.1 , 'B2')
-plt.plot(B1[0], B1[1], 'o')
-plt.text(B1[0] * (1 ), B1[1] * (1)+0.1 , 'B1')
-plt.xlabel('$x$')
-plt.ylabel('$y$')
+plt.axis('equal')
 plt.legend(loc='best')
-plt.grid() # minor
-
+plt.grid()
 plt.show()
